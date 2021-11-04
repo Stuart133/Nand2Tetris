5de89283ed10d56f0e1e1b1c36de72974688a2aa
Stuart Reid <stuart1334@gmail.com>
2021-11-04 08:46:59 +0000

    Multiply assembly

--- projects/04/mult/Mult.asm
+++ projects/04/mult/Mult.asm
@@ -9,4 +9,40 @@
 // This program only needs to handle arguments that satisfy
 // R0 >= 0, R1 >= 0, and R0*R1 < 32768.
 
-// Put your code here.
+  // Set total & loop counter to 0
+  @total
+  M=0
+  @n
+  M=0
+  @R2
+  M=0
+(LOOP)
+  // if n == R1 goto END
+  @n
+  D=M
+  @R1
+  D=D-M
+  @STOP
+  D;JEQ  
+  // Add R0 to total
+  @total
+  D=M
+  @R0
+  D=D+M
+  @total
+  M=D
+  // n++
+  @n
+  M=M+1
+  // goto LOOP
+  @LOOP
+  0;JMP
+(STOP)
+  // R2 = total
+  @total
+  D=M
+  @R2
+  M=D
+(END)
+  @END
+  0;JMP
