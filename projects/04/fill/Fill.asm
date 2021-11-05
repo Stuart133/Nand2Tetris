206e938c5cb5367e160dd24483bff0b8c145b6d8
Stuart Reid <stuart1334@gmail.com>
2021-11-05 09:06:10 +0000

    Add screen fill assembly program

--- projects/04/fill/Fill.asm
+++ projects/04/fill/Fill.asm
@@ -11,4 +11,61 @@
 // "white" in every pixel;
 // the screen should remain fully clear as long as no key is pressed.
 
-// Put your code here.
+  // current is the next screen word to be operated on
+  @SCREEN
+  D=A
+  @current
+  M=D
+  @8192
+  D=D+A
+  @max
+  M=D
+(LOOP)
+  // Is there a key press? FILL if yes, EMPTY if not
+  @KBD
+  D=M
+  @EMPTY
+  D;JEQ
+  @FILL
+  0;JMP
+// Fill current word
+(FILL)
+  @current
+  A=M
+  M=-1
+  @current
+  M=M+1
+  // If current == max goto reset
+  D=M
+  @max
+  D=D-M
+  @RESET
+  D;JEQ
+  // goto LOOP
+  @LOOP
+  0;JMP
+// Empty the current word
+(EMPTY)
+  @current
+  A=M
+  M=0
+  @current
+  M=M+1
+  // If current == max goto reset
+  D=M
+  @max
+  D=D-M
+  @RESET
+  D;JEQ
+  // goto LOOP
+  @LOOP
+  0;JMP
+// Reset word counter to first word
+(RESET) 
+  @SCREEN
+  D=A
+  @current
+  M=D
+  // Back to main loop
+  @LOOP
+  0;JMP
