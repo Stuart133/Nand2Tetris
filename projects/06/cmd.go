71890b39f5d5a032f7c7cc6a428e0ec89065ffb6
Stuart Reid <stuart1334@gmail.com>
2021-11-09 20:05:41 +0000

    Add advance functionality

--- projects/06/cmd.go
+++ projects/06/cmd.go
@@ -2,20 +2,28 @@ package main
 
 import (
 	"fmt"
-	"hack-assembler/pkg/parser"
 	"os"
+	"strings"
+
+	"hack-assembler/pkg/parser"
 )
 
 func main() {
 	path := os.Args[1]
-	if path[len(path)-3:] != "asm" {
+	if !strings.HasSuffix(path, "asm") {
 		fmt.Println("File specified must be a *.asm file")
 		os.Exit(1)
 	}
 
-	_, err := parser.Load(path)
+	p, err := parser.Load(path)
 	if err != nil {
 		fmt.Println("There was an error loading the assembly file: $v", err)
 		os.Exit(1)
+	}
+
+	fmt.Printf("%d: %s\n", 0, p.GetLine())
+	for i := 0; p.HasMoreLines(); i++ {
+		p.Advance()
+		fmt.Printf("%d: %s\n", i, p.GetLine())
 	}
 }
--- projects/06/pkg/parser/parser.go
+++ projects/06/pkg/parser/parser.go
@@ -1,7 +1,6 @@
 package parser
 
 import (
-	"fmt"
 	"io/ioutil"
 	"strings"
 )
@@ -17,14 +16,32 @@ func Load(path string) (Parser, error) {
 		return Parser{}, err
 	}
 
-	lines := strings.Split(string(content), "/n")
+	rawLines := strings.Split(string(content), "\n")
 
-	for i := range lines {
-		fmt.Printf("Line: %s\n", lines[i])
+	// In leiu of a full scanner pass remove comments/whitespace here
+	lines := make([]string, 0)
+	for i := range rawLines {
+		if !strings.HasPrefix(rawLines[i], "//") && !(len(strings.TrimSpace(rawLines[i])) == 0) {
+			lines = append(lines, rawLines[i])
+		}
 	}
 
 	return Parser{
 		lines:    lines,
 		position: 0,
 	}, nil
+}
+
+func (p *Parser) GetLine() string {
+	return p.lines[p.position]
+}
+
+func (p *Parser) Advance() {
+	if p.HasMoreLines() {
+		p.position++
+	}
+}
+
+func (p *Parser) HasMoreLines() bool {
+	return len(p.lines)-1 != p.position
 }
