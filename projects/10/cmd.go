33b48fea1d1aac0e4499b41133a14878cbf01187
Stuart Reid <stuart@circleci.com>
2021-11-28 16:58:18 +0000

    Add basic IO

--- projects/10/cmd.go
+++ projects/10/cmd.go
@@ -1,5 +1,29 @@
 package main
 
+import (
+	"compiler/pkg/scanner"
+	"fmt"
+	"io/ioutil"
+	"os"
+	"strings"
+)
+
 func main() {
+	path := os.Args[0]
+
+	if strings.HasSuffix(path, ".jack") {
+		c, err := ioutil.ReadFile(path)
+		if err != nil {
+			fmt.Printf("Could not open file: %v", err)
+			os.Exit(1)
+		}
+
+		s := scanner.NewScanner(string(c))
+		tokens := s.ScanTokens()
+
+		for _, t := range tokens {
+			fmt.Printf("%v\n", t)
+		}
+	}
 
 }
