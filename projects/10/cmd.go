0eeecdceda7c3a01379909e92d4bfa635d52751e
Stuart Reid <stuart@circleci.com>
2021-11-29 16:43:01 +0000

    Add printer code

--- projects/10/cmd.go
+++ projects/10/cmd.go
@@ -10,21 +10,52 @@ import (
 
 func main() {
 	path := os.Args[1]
+	fi, err := ioutil.ReadDir(path)
+	if err != nil {
+		fmt.Printf("Could not read directory: %v", err)
+	}
 
-	if strings.HasSuffix(path, ".jack") {
-		c, err := ioutil.ReadFile(path)
+	for _, f := range fi {
+		if !strings.HasSuffix(f.Name(), ".jack") {
+			continue
+		}
+
+		c, err := ioutil.ReadFile(fmt.Sprintf("%s\\%s", path, f.Name()))
 		if err != nil {
 			fmt.Printf("Could not open file: %v", err)
 			os.Exit(1)
 		}
 
-		fmt.Println(string(c))
-
 		s := scanner.NewScanner(string(c))
 		tokens := s.ScanTokens()
 
-		for _, t := range tokens {
-			fmt.Printf("%v\n", t)
+		oPath := fmt.Sprintf("%s\\%sT-gen.xml", path, getFileName(f.Name()))
+		err = saveFile(oPath, tokens)
+		if err != nil {
+			fmt.Printf("There was an error writing the the output file: %v\n", err)
+			os.Exit(1)
 		}
 	}
+}
+
+func saveFile(path string, tokens []scanner.Token) error {
+	f, err := os.Create(path)
+	if err != nil {
+		return err
+	}
+	defer f.Close()
+
+	err = scanner.WriteXml(tokens, f)
+	if err != nil {
+		return err
+	}
+
+	return nil
+}
+
+func getFileName(path string) string {
+	sp := strings.Split(path, "\\")
+	sp = strings.Split(sp[len(sp)-1], ".")
+
+	return sp[0]
 }
