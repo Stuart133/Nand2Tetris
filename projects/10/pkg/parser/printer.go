8fcf194743e371f2d3e78373e3b55a4326764a27
Stuart Reid <stuart@circleci.com>
2021-12-02 20:13:09 +0000

    Fix some output bugs

--- projects/10/pkg/parser/printer.go
+++ projects/10/pkg/parser/printer.go
@@ -8,14 +8,13 @@ import (
 func WriteXml(stmts []SyntaxNode, w io.Writer, i int) error {
 	for _, node := range stmts {
 		if len(node.Nodes) == 0 {
-			l := indent(i, fmt.Sprintf("<%s> %s </%s>\n", node.TypeName, node.Lexeme, node.TypeName))
-
+			l := format(node, i)
 			_, err := w.Write([]byte(l))
 			if err != nil {
 				return err
 			}
 
-			return nil
+			continue
 		}
 
 		l := indent(i, fmt.Sprintf("<%s>\n", node.TypeName))
@@ -40,10 +39,19 @@ func WriteXml(stmts []SyntaxNode, w io.Writer, i int) error {
 	return nil
 }
 
+func format(n SyntaxNode, i int) string {
+	if n.Lexeme == "" {
+		lower := indent(i, fmt.Sprintf("</%s>\n", n.TypeName))
+		return indent(i, fmt.Sprintf("<%s>\n%s", n.TypeName, lower))
+	} else {
+		return indent(i, fmt.Sprintf("<%s> %s </%s>\n", n.TypeName, n.Lexeme, n.TypeName))
+	}
+}
+
 func indent(indent int, c string) string {
 	t := ""
 	for i := 0; i < indent; i++ {
-		t += "\t"
+		t += "  "
 	}
 
 	return fmt.Sprintf("%s%s", t, c)
